93403204d957053e8523e616424f72396a87ddc9
wyb <happycodehouse@gmail.com>
2025-01-05 17:06:23 +0900

    start reset css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
 
-  return (
-    <></>
-  )
+    return (
+        <>
+            <header>
+                <div className={'header-inner'}>
+                    <h1>basic features blog</h1>
+                </div>
+            </header>
+            <div className={'container'}>
+
+            </div>
+        </>
+    )
 }
 
 export default App
